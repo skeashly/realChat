2571a7a3a465d82ce9efe0265119bcacb699ccac
Sarah Keashly <sarahejohnson89@gmail.com>
2023-10-16 16:35:19 -0700

    Set up basic express server with socket.io and html file with input field for users. Link up socket.io.

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
